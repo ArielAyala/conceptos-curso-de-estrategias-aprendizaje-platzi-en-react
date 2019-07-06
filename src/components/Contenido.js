8c1656ae6f6ffd0c545b93e9d15b612f9dd5da66
ArielAyala <arielayalamail@gmail.com>
2019-07-05 21:06:51 -0400

    Agreagado de menu

--- src/components/Contenido.js
+++ src/components/Contenido.js
@@ -11,7 +11,35 @@ class Contenido extends Component {
         </div>
 
         <div className="container contenido border">
-          <h1>Contenido</h1>
+          <h3 className="titulo-centrado">Titulo de item</h3>
+          <div className="container-menu">
+            <div id="list-example" className="list-group">
+              <a
+                className="list-group-item list-group-item-action"
+                href="#list-item-1"
+              >
+                Item 1
+              </a>
+              <a
+                className="list-group-item list-group-item-action"
+                href="#list-item-2"
+              >
+                Item 2
+              </a>
+              <a
+                className="list-group-item list-group-item-action"
+                href="#list-item-3"
+              >
+                Item 3
+              </a>
+              <a
+                className="list-group-item list-group-item-action"
+                href="#list-item-4"
+              >
+                Item 4
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
