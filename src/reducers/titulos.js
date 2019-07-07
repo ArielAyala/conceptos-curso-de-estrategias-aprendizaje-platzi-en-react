e23e99a836c3d56d01129522709cd58c7be559a5
ArielAyala <arielayalamail@gmail.com>
2019-07-07 17:35:41 -0400

    Agregado de caso 'Traer_Titulos' al reducer

--- src/reducers/titulos.js
+++ src/reducers/titulos.js
@@ -1,9 +1,13 @@
+import { TRAER_TITULOS } from "../types/titulos";
+
 const INITIAL_STATE = {
   titulos: []
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case TRAER_TITULOS:
+      return { ...state, titulos: action.payload };
     default:
       return state;
   }
