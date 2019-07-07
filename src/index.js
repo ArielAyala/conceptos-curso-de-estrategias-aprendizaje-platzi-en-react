75175c75c81ba3a47998427edcbd199d3bb7ed34
ArielAyala <arielayalamail@gmail.com>
2019-07-07 17:29:21 -0400

    Agregando reducer y probanto método getState

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
-import {createStore, applyMiddleware} from 'redux';
-import reduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducers from "./reducers/combineReducers";
 
 const store = createStore(
-    null,//Reducers
-    {},//Estado inicial
-    applyMiddleware(reduxThunk)
-}
+  reducers, //Reducers
+  {}, //Estado inicial
+  applyMiddleware(reduxThunk)
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
+//console.log(store.getState())
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/combineReducers.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import titulos from "./titulos";
+
+export default combineReducers({
+  titulos
+});
