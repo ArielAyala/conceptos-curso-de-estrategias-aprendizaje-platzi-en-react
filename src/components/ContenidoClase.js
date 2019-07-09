56a2f1e352b1a54057f998cfc4fd84dbe5c989ab
Ariel <arielayalamail@gmail.com>
2019-07-09 07:08:00 -0400

    Creado de componente de Clase 2

--- src/components/ContenidoClase.js
+++ src/components/ContenidoClase.js
@@ -2,12 +2,14 @@ import React, { Component, Fragment } from "react";
 
 //Clases
 import Clase1 from "./clases/Clase1";
+import Clase2 from "./clases/Clase2";
 
 class ContenidoClase extends Component {
   render() {
     return (
       <Fragment>
         <Clase1 />
+        <Clase2 />
       </Fragment>
     );
   }
---
+++ src/components/clases/Clase2.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+/*Tipos de pensamiento*/
+const Clase2 = () => (
+  <div className="clase">
+    <p>
+      La <b>neurociencia</b> es el campo de la ciencia que se encarga de
+      estudiar el sistema nervioso.
+    </p>
+    <p>Según la doctora Barbara Oakley existen dos tipos de pensamientos</p>
+    <p>
+      - <b>Enfocado</b>: Cuando nos concentramos en un largo periodo de tiempo
+      ininterrumpido. Podrías concentrarte tratando de aprender algo con muchas
+      horas. Este tipo de pensamiento es adecuado cuando ya tienes una{" "}
+      <b>base de conocimiento</b>.
+    </p>
+    <p>
+      - <b>Difuso</b>: cuando nos encontramos ante <b>problemas nuevos</b>.
+      Cuando partes de cero y quieres aprender algo nuevo. Se trata de{" "}
+      <b>estudiar con interrupciones de tiempo</b>.
+    </p>
+  </div>
+);
+
+export default Clase2;
