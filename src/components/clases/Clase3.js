c5edde8c27eabd3dcae356f2d8832c79ed02ea51
Ariel <arielayalamail@gmail.com>
2019-07-11 06:34:58 -0400

    Agregado de clase 3 Etapas del aprendizaje

---
+++ src/components/clases/Clase3.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+/*Que es el aprendizaje*/
+const Clase3 = () => (
+  <div className="clase">
+    <p>
+      En la <b>entrada</b> de información tenemos dos tipos de experiencia:
+    </p>
+    <ul>
+      <li>
+        <b>Experiencia concreta (EC):</b> Es un perfil que le gusta involucrarse
+        y de tener la experiencia.
+      </li>
+      <li>
+        <b>Observación reflexiva (OR):</b> Es un perfil que le gusta comenzar su
+        aprendizaje por medio de la lectura o la teoría.
+      </li>
+    </ul>
+    <p>
+      La <b>salida</b> depende un poco de cómo fue la entrada:
+    </p>
+    <ul>
+      <li>Conceptualización abstracta (CA).</li>
+      <li>Experimentación activa (EA).</li>
+    </ul>
+  </div>
+);
+
+export default Clase3;
