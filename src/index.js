2cc4821515e1a21a356a4915d7ee6b024e649c66
ArielAyala <arielayalamail@gmail.com>
2019-07-10 20:04:48 -0400

    Se agrega el emhancer para desarrollo

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducers from "./reducers/combineReducers";
 
+// const store = createStore(
+//   reducers, //Reducers
+//   {}, //Estado inicial
+//   applyMiddleware(reduxThunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducers, //Reducers
-  {}, //Estado inicial
-  applyMiddleware(reduxThunk)
+  reducers,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(reduxThunk))
 );
 
 //console.log(store.getState())
