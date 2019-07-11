5cc62b796f674df628fd68b5957921bb1c61e899
Ariel <arielayalamail@gmail.com>
2019-07-11 06:35:23 -0400

    Agregando de clase 4 - Tipos de estudianes

---
+++ src/components/clases/Clase4.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+/*Que es el aprendizaje*/
+const Clase4 = () => (
+  <div className="clase">
+    <p>
+      <b>Alumnos activos:</b> Son entusiastas y les apasiona las cosas nuevas.
+      Son este tipo de alumnos que actúa antes de pensar. Por lo general,
+      aprenden mejor si tienen una actividad que consideren un desafío. Realizan
+      actividades cortas de resultado inmediatos.
+    </p>
+    <br />
+    <p>
+      <b>Alumnos reflexivos:</b> Aprenden más cuando pueden observar
+      detenidamente la información que les rodea, cuando les ofrecen tiempo de
+      analizar y reflexionar antes de actuar y pueden pasar desapercibidos.
+    </p>
+    <br />
+    <p>
+      <b>Alumnos teóricos:</b> Aprenden mejor cuando les presentan modelos
+      objetivos, teorías y sistemas; cuando pueden investigar y rastrear
+      información. A este tipo de alumnos les gusta mucho la forma secuencial de
+      pensamiento, la forma lógica para llegar a la resolución del problema.
+    </p>
+    <p>
+      Les cuesta aprender cuando se les presentan actividades que consideran
+      confusas o inciertas, cuando deben desarrollar actividades muy subjetivas
+      o emocionales y cuando tienen que trabajar sin un marco teórico de
+      referencia.
+    </p>
+    <p>
+      <b>Alumnos pragmáticos: </b>Aprenden mejor cuando se les ofrecen
+      actividades en las que pueden relacionar las teorías con situaciones
+      prácticas y cuando pueden poner en práctica lo que deben aprender.
+    </p>
+    <p>
+      Les cuesta aprender cuando se presentan actividades que no se relacionan
+      con la realidad y cuando no pueden relacionar la información con
+      situaciones prácticas.
+    </p>
+  </div>
+);
+
+export default Clase4;
