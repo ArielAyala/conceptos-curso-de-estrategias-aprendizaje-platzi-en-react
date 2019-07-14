f10d80f9bbb7f1b5d29de98f9840eac6d64c1696
ArielAyala <arielayalamail@gmail.com>
2019-07-14 10:35:46 -0400

    Agregado de clase 15 - Evaluar mi conocimiento

--- src/components/clases/Clase15.js
+++ src/components/clases/Clase15.js
@@ -2,10 +2,22 @@ import React from "react";
 
 //Evaluar mi conocimiento
 const Clase15 = () => (
-  <div className="clase">
+  <div className="clase text-justify">
     <p>
       <b>Evaluar</b> conocimiento te da la oportunidad de corroborar lo que
       aprendiste, evaluar qué tanto has aprendido.
+    </p>
+    <p>
+      Por lo general lo mejor es que estas evaluaciones sean diseñadas y
+      aplicadas por otra persona que no seas tu.
+    </p>
+    <p>
+      Las evaluaciones deben ser diseñadas con ejemplos que te confronten a la
+      resolución de otro tipo de problemas diferentes a lo que aprendiste.
+    </p>
+    <p>
+      Esto también te permitirá completar tu conocimiento y darte la posibilidad
+      de aprender a aplicarlo en más áreas.
     </p>
   </div>
 );
