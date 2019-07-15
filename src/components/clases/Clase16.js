080cb2c019a6fbc8ad761b49e6608880cb3e4c6c
ArielAyala <arielayalamail@gmail.com>
2019-07-15 19:03:58 -0400

    Agregando de espacio en clase 16

---
+++ src/components/clases/Clase16.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//El diálogo me conduce a la reflexión de mi aprendizaje
+const Clase16 = () => (
+  <div className="clase text-justify">
+    <p>
+      El argumento es una reflexión, una respuesta construida en base a la
+      razón, defender una postura con el mayor racionamiento posible y así sea
+      aceptado por la otra persona.
+    </p>
+    <br />
+    <p>
+      Con estos argumentos lo que queremos es conseguir la aceptación de la otra
+      persona de manera racional. También desarrollamos capacidades
+      lingüísticas, incrementamos en una 50% la adquisición del conocimientos
+      para el día siguiente.
+    </p>
+  </div>
+);
+
+export default Clase16;
